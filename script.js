a6b84c95bce40f168104f865a7e9f2a2c6f901e3
OliverKalitecnis <97357784+OliverKalitecnis@users.noreply.github.com>
2023-07-12 14:46:43 -0600

    Update script.js
    primera descripción de java para la galería

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Ejemplo de datos de imágenes
+const images = [
+  { category: 'oleo', src: 'oleo1.jpg' },
+  { category: 'oleo', src: 'oleo2.jpg' },
+  { category: 'acrilica', src: 'acrilica1.jpg' },
+  { category: 'acrilica', src: 'acrilica2.jpg' },
+  // Agrega más imágenes y categorías según tus necesidades
+];
 
+//Función para generar las imágenes en la galería
+function generateGallery() {
+  const galleryContainer = document.getElementById('gallery');
+
+  images.forEach(image => {
+    const categoryContainer = document.querySelector('category[data-category="${image.category}"]');
+    const imageElement = document.createElement('img');
+    imageElement.src = image.src;
+    categoryContainer.querySelector('.image-container').appendChild(imageElement);    
+  });        
+}
+
+
+// Llama a la función para generar la galería al cargar la página
+window.addEventListener('load' , generateGallery);
